89e0c3b47f1b50c19243a6b6e482894b85ac4fb6
rodrigozura <rodrigozura@gmail.com>
2025-06-04 15:46:27 -0300

    center footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,12 @@
 export default function Footer() {
   return (
     <footer className="bg-green-800 text-white py-4 mt-auto fixed bottom-0 w-full">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="text-sm mb-2 md:mb-0">
-            © 2024 Simulador de Lobesia Botrana. Todos los derechos reservados.
-          </div>
-          <div className="text-sm">
-            Desarrollado por el Grupo 4K3 - Simulacion - UTN-FRT
-          </div>
+      <div className="container mx-auto flex flex-col items-center sm:flex-row sm:justify-between sm:items-center gap-2 text-center sm:text-left">
+        <div>
+          <p>© 2024 Simulador de Lobesia Botrana. Todos los derechos reservados.</p>
+        </div>
+        <div>
+          <p>Desarrollado por el Grupo 4K3 - Simulacion - UTN-FRT</p>
         </div>
       </div>
     </footer>
